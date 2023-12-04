6d5c15e6810d7fd7466054e16e51119255769c97
samadeep <64741761+samadeep@users.noreply.github.com>
2023-12-04 23:51:15 +0530

    Create streamlit_chatbot.py

---
+++ streamlit_chatbot.py
@@ -0,0 +1,41 @@
+# To run -> pip3 install streamlit -> streamlit run streamlit_chatbot.py
+
+import streamlit as st
+import random
+import time
+from chat import chat_function
+
+st.title("Drug Abuse Relief Chatbot")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("I am here to help"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        assistant_response = chat_function(prompt)
+        # Simulate stream of response with milliseconds delay
+        for chunk in assistant_response.split():
+            full_response += chunk + " "
+            time.sleep(0.05)
+            # Add a blinking cursor to simulate typing
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
